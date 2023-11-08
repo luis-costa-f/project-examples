b44ce787ca77302907ab265625c03c43f2ba1d7c
Luis Costa <luiscosta.uva@gmail.com>
2023-11-07 23:27:08 -0300

    chore: added initial project calculadora

---
+++ _1_calculadora/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Calculator from './components/Calculator'
+
+const App = () => {
+    return (
+        <div>
+            <Calculator />
+        </div>
+    )
+}
+
+export default App
+
+
---
+++ _1_calculadora/src/components/Calculator.tsx
@@ -0,0 +1,14 @@
+const Calculator = () => {
+    return (
+        <>
+            <div className="calculator">
+                <div className="complete-operation"> 3 + 2</div>
+                <div className="display"> 5</div>
+                <div className="buttons"></div>
+                <button>=</button>
+            </div>
+        </>
+    );
+}
+
+export default Calculator
