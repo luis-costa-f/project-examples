f1610600bca9dd409553681be6992bdeaea9c7ed
Luis Costa <luiscosta.uva@gmail.com>
2023-11-10 19:43:55 -0300

    added initial project CurrencyConverter

---
+++ _3_conversor_moeda/src/components/CurrencyConverter.tsx
@@ -0,0 +1,11 @@
+import "./CurrencyConverter.css"
+
+const CurrencyConverter = () => {
+    return (
+        <div>
+            CurrencyConverter
+        </div>
+    )
+}
+
+export default CurrencyConverter
