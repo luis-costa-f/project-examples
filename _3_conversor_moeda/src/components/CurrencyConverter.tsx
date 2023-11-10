04b0a1f23c6aa265db957a31f4314efefcc28797
Luis Costa <luiscosta.uva@gmail.com>
2023-11-10 19:54:35 -0300

    added stylish for currencyConverter

--- _3_conversor_moeda/src/components/CurrencyConverter.tsx
+++ _3_conversor_moeda/src/components/CurrencyConverter.tsx
@@ -2,8 +2,19 @@ import "./CurrencyConverter.css"
 
 const CurrencyConverter = () => {
     return (
-        <div>
-            CurrencyConverter
+        <div className="converter">
+            <h2>Conversor de Moedas</h2>
+            <input type="number" />
+            <span>Selecione as moedas</span>
+            <select>
+                <option value="BRL">BRL</option>
+            </select>
+            <span>para</span>
+            <select>
+                <option value="USD">USD</option>
+            </select>
+            <h2>BRL USD</h2>
+            <p>20 BRL valem 5 USD</p>            
         </div>
     )
 }
