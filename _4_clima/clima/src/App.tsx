79b0c9aa5c70db841bab17ef125fad7d45187433
Luis Costa <luiscosta.uva@gmail.com>
2023-11-13 00:16:39 -0300

    feature: search data in the API

--- _4_clima/clima/src/App.tsx
+++ _4_clima/clima/src/App.tsx
@@ -3,6 +3,7 @@ import { Titulo } from './AppStyles'
 import Busca from './components/Busca'
 import ClimaAtual from './components/ClimaAtual'
 import Previsao from './components/Previsao'
+import axios from 'axios'
 
 const App = (): JSX.Element => {
     const [cidade, setCidade] = useState<string>("");
@@ -11,10 +12,21 @@ const App = (): JSX.Element => {
 
     const apiKey = import.meta.env.VITE_API_KEY;
 
+    const buscarClima = async() => {
+        try {
+            const respostaClima = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}`)
+            setClima(respostaClima.data);
+        } catch (error) {
+            console.log("Erro ao buscar clima: ", error)
+        }
+    }
+
+    console.log(clima)
+
     return (
         <div>
             <Titulo>Condições Climáticas</Titulo>
-            <Busca />
+            <Busca cidade={cidade} setCidade={setCidade} buscarClima={buscarClima} />
             <ClimaAtual />
             <Previsao />
         </div>
--- _4_clima/clima/src/components/Busca.tsx
+++ _4_clima/clima/src/components/Busca.tsx
@@ -1,8 +1,17 @@
-const Busca = () => {
+import { ChangeEvent } from "react";
+
+interface IBusca {
+    cidade: string;
+    setCidade: (value: string) => void;
+    buscarClima: () => void;
+}
+
+
+const Busca = ({ cidade, buscarClima, setCidade }: IBusca) => {
     return (
         <div>
-            <input type="text" />
-            <button>Buscar</button>
+            <input type="text" value={cidade || ""} onChange={(e: ChangeEvent<HTMLInputElement>) => setCidade(e.currentTarget.value)} />
+            <button onClick={() => buscarClima()}>Buscar</button>
         </div>
     )
 }
