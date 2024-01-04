7ea82569657d54f2a039972be6dd9e8f05b61d64
Luis Costa <luiscosta.uva@gmail.com>
2024-01-04 13:57:28 -0300

    feature: added css style and created intial componente Toolbar

--- _7_markdown_viewer/markdown_viewer/src/App.tsx
+++ _7_markdown_viewer/markdown_viewer/src/App.tsx
@@ -1,9 +1,42 @@
+import { marked } from "marked";
+import { useEffect, useRef, useState } from "react";
+import Toolbar from "./components/Toolbar";
+
 const App = () => {
-  return (
-    <div>
-      Markdown
-    </div>
-  )
+
+    const [text, setText] = useState<string>(
+        localStorage.getItem("markdownText") || "# Olá, eu sou feito de Markdown"
+    );
+
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    const renderText = () => {
+        return { __html: marked(text) }
+    }
+
+    const handleChangeTextArea = (newText: string): void => {
+        setText(newText)
+    }
+
+    
+
+    useEffect(() => {
+        localStorage.setItem("markdownText", text);
+    }, [text])
+
+    return (
+        <div>
+            <div className="app-container">
+                <Toolbar />
+                <textarea
+                    ref={textAreaRef}
+                    value={text}
+                    onChange={(e) => handleChangeTextArea(e.target.value)}
+                ></textarea>
+                <div dangerouslySetInnerHTML={renderText()} />
+            </div>
+        </div>
+    )
 }
 
 export default App
---
+++ _7_markdown_viewer/markdown_viewer/src/components/Toolbar.tsx
@@ -0,0 +1,15 @@
+const Toolbar = () => {
+    return (
+        <div className="toolbar">
+            <button>h1</button>
+            <button>h2</button>
+            <button>Bold</button>
+            <button>Italic</button>
+            <button>Link</button>
+            <button>Code block</button>
+            <button>Horizontal Line</button>
+        </div>
+    )
+}
+
+export default Toolbar
