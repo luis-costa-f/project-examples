781b9c378abaa9b82dee8ab002663f5ea390ee89
Luis Costa <luiscosta.uva@gmail.com>
2024-02-01 15:48:45 -0300

    feature: added initial files.

---
+++ _8_relogio_mundial/src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Relógio Mundial</h1>
+      <select>
+        <option value="" disabled selected>
+          Selecione um fuso horário
+        </option>
+      </select>
+    </div>
+  )
+}
+
+export default App
---
+++ _8_relogio_mundial/src/components/TimezoneClock/index.tsx
@@ -0,0 +1,10 @@
+const TimezoneClock = () => {
+    return (
+        <div>
+            <h2>Timezone</h2>
+            <h3>Tempo</h3>
+        </div>
+    )
+}
+
+export default TimezoneClock
