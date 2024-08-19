3e6849d292014578d85bd1a5fd3834d637b93f35
Luis Costa <luiscosta.uva@gmail.com>
2024-08-18 21:56:10 -0300

    added: first components and added some configurations about quality code

---
+++ 1_calculadora/calculadora/eslint.config.js
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: {
+            globals: globals.browser,
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ...pluginReact.configs.flat.recommended,
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            "react/react-in-jsx-scope": "off", // Desativa a regra se não for necessário importar o React
+            "indent": ["error", 4], // Configura a indentação para 4 espaços
+            "semi": ["error", "always"], // Garante que o ponto e vírgula seja obrigatório
+        },
+    },
+];
---
+++ 1_calculadora/calculadora/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import Calculator from './components/Calculator';
+
+const App = () => {
+    return <Calculator />;
+};
+
+export default App;
